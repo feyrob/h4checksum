927ed5733a358ff5b72b03ad7dfe603f506067c3
Robert Fey <feyrob@gmail.com>
2011-12-15 01:52:49 +0100

    clean up build scripts and file naming

--- compile_run.sh
+++ compile_run.sh
@@ -11,6 +11,8 @@ rm -rf ./ex/*
 scons
 
 cp ./build_std/executable ./ex/$PROJECT_NAME 
-cp ./build_std/lib_shared.so ./lib/lib$PROJECT_NAME.so 
-cp ./build_std/lib_static.a ./lib/lib$PROJECT_NAME.a
+#cp ./build_std/lib_shared.so ./lib/lib$PROJECT_NAME.so 
+#cp ./build_std/lib_static.a ./lib/lib$PROJECT_NAME.a
+cp ./build_std/*.so ./lib/lib$PROJECT_NAME.so 
+cp ./build_std/*.a ./lib/lib$PROJECT_NAME.a
 ./ex/$PROJECT_NAME --file ./data/test_1.png
